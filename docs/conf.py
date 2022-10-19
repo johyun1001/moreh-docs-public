0107435ce520303c1665f6cca22749a97146bdbd
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2022-10-19 22:15:07 +0900

    copybtn

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,12 @@ release = '2022'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser'  
+extensions = [
+
     'sphinx_copybutton'
-    ]
+
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
