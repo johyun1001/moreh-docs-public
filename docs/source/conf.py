9fe555ded5658fc3901292c6f1a78d1aeecd50fa
johyun1001 <115516629+johyun1001@users.noreply.github.com>
2022-10-25 19:39:03 +0900

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [    'sphinx_rtd_theme','myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -25,5 +25,25 @@ language = 'ko'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+
+#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_material"
 html_static_path = ['_static']
+
+html_theme_options = {
+    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'orange',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
